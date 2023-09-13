1c52094071dab5696ed4ce94d75ec9caf3109803
Dr Code <dice@Dice>
2023-09-13 14:02:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+import bg from '../images/kali.png'
+const Hero = () => {
+  return (
+    <div className='w-full flex min-h-screen'>
+      <img className='w-full' src={bg} alt='image'/>
+    </div>
+  )
+}
+
+export default Hero
