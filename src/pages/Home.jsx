6aa458f53cc9fd6a53a8f5adbada12f16358918c
Dr Code <dice@Dice>
2023-09-13 15:14:31 +0200

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import backgroundImage from "../images/sun.jpg";
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const HandleMenu = () => {
+    setOpen(!open);
+  };
+
   const divStyle = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
@@ -12,10 +18,17 @@ const Home = () => {
       style={divStyle}
     >
       <div className="flex w-full py-6 justify-between">
-        <a className="text-[25px] text-white">Kali-Docs</a>
+        <a className="text-[25px] font-semibold text-white">Kali-Docs</a>
         <li className="flex gap-4 items-baseline">
-          <a className="text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">Docs</a>
-          <a className="text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">Donate</a>
+          <a className="md:flex hidden text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+            Docs
+          </a>
+          <a className="md:flex hidden text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+            Donate
+          </a>
+          <button onClick={HandleMenu} className="md:hidden lg:hidden sm:flex">
+            <ion-icon size="large" name="grid-outline"></ion-icon>
+          </button>
         </li>
       </div>
     </div>
