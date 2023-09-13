ed6111f06cdd6832c5bdca557c2e7829b40ba1d6
Dr Code <dice@Dice>
2023-09-13 19:16:38 +0200

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,15 +16,15 @@ const Home = () => {
   return (
     <div className="w-full min-h-screen flex flex-col px-6" style={divStyle}>
       <div className="flex w-full py-4 justify-between items-center">
-        <a className="text-[25px] font-semibold text-white">Kali-Docs</a>
+        <a className="text-[25px] cursor-pointer font-semibold text-white">Kali-Docs</a>
         <li className="flex gap-4 items-baseline">
-          <a className="md:flex hidden text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <a className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
             News
           </a>
-          <a className="md:flex hidden text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <a className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
             Docs
           </a>
-          <a className="md:flex hidden text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <a className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
             Donate
           </a>
           <button
@@ -59,13 +59,19 @@ const Home = () => {
               type="text"
               id="searchInput"
               placeholder="Search..."
-              className="py-2 px-2 lg:w-[350px] md:w-[350px] w-full focus:outline-none focus:ring-0 bg-white"
+              className="py-2 px-2 cursor-pointer lg:w-[350px] md:w-[350px] w-full focus:outline-none focus:ring-0 bg-white"
             />
+            <a className="cursor-pointer">
             <ion-icon size="large" name="search-outline"></ion-icon>
+            </a>
           </div>
         </div>
       </div>
-      <div className="py-4 text-white text-center">Footer</div>
+      <div className="w-full flex py-4 text-white justify-between">
+        <a>DiCE Labs</a>
+        <a>Copyright @ 2023</a>
+        <a className="text-white cursor-pointer"><ion-icon size="large" name="logo-facebook"></ion-icon></a>
+      </div>
     </div>
   );
 };
