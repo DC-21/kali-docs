41a0029b6f57721e7bc1c350ad2f38ae56f1e054
Dr Code <dice@Dice>
2023-09-13 19:38:22 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 };
 
