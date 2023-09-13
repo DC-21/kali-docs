f2b844ddfe3e42de30d3554f894621fab9bd8e4f
Dr Code <dice@Dice>
2023-09-13 15:02:18 +0200

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
   };
   return (
     <div
-      className="w-full min-h-screen flex bg-blue-900 px-6 justify-center"
+      className="w-full min-h-screen flex px-6 justify-center"
       style={divStyle}
     >
-      <div className="flex w-full py-6 justify-center">
-        <a>Kali-Docs</a>
-        <li>
-          <a>Docs</a>
-          <a>Donate</a>
+      <div className="flex w-full py-6 justify-between">
+        <a className="text-[25px] text-white">Kali-Docs</a>
+        <li className="flex gap-4 items-baseline">
+          <a className="text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">Docs</a>
+          <a className="text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">Donate</a>
         </li>
       </div>
     </div>
