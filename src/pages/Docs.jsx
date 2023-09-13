888b37b9c46f717f63a0a014cea883d59c9c6899
Dr Code <dice@Dice>
2023-09-13 20:40:25 +0200

    first commit

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -11,13 +11,13 @@ const Docs = () => {
         <div className="flex w-full py-4 justify-between items-center">
         <Link to="/" className="text-[25px] cursor-pointer font-semibold text-blue-900">Kali-Docs</Link>
         <li className="flex gap-4 items-baseline">
-          <Link to="/news" className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <Link to="/news" className="md:flex hidden cursor-pointer text-[20px] text-blue-900 hover:bg-[#5295e0] px-2 py-2 rounded">
             News
           </Link>
-          <Link to="/docs" className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <Link to="/docs" className="md:flex hidden cursor-pointer text-[20px] text-blue-900 hover:bg-[#5295e0] px-2 py-2 rounded">
             Docs
           </Link>
-          <a href='https://bmc.link/cholahkuboQ' className="md:flex hidden cursor-pointer text-[20px] text-white hover:bg-[#5295e0] px-2 py-2 rounded">
+          <a href='https://bmc.link/cholahkuboQ' className="md:flex hidden cursor-pointer text-[20px] text-blue-900 hover:bg-[#5295e0] px-2 py-2 rounded">
             Donate
           </a>
           <button
@@ -44,6 +44,25 @@ const Docs = () => {
             )}
           </button>
         </li>
+      </div>
+      <div className="flex-grow flex justify-center items-center py-6">
+        <div className="w-full justify-center items-center flex">
+          <div htmlFor="searchInput" className="flex text-black gap-4 justify-center items-center lg:w-[450px] md:w-[450px] w-full lg:py-5 md:py-5 py-3 px-2 bg-white rounded-full">
+            <input
+              type="text"
+              id="searchInput"
+              placeholder="Search..."
+              className="py-2 px-2 cursor-pointer lg:w-[350px] md:w-[350px] w-full focus:outline-none focus:ring-0 bg-white"
+            />
+            <a className="cursor-pointer">
+            <ion-icon size="large" name="search-outline"></ion-icon>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex py-4 text-blue-900 justify-between items-center">
+        <a>DiCE Labs||Copyright @ 2023</a>
+        <a href='https://github.com/dc-21' className="text-blue-900 cursor-pointer"><ion-icon size="large" name="logo-github"></ion-icon></a>
       </div>
     </div>
   )
