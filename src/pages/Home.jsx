8008cafef66b02515eeb0c07747f67d2b8c3b11a
Dr Code <dice@Dice>
2023-09-13 14:56:04 +0200

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
+import backgroundImage from "../images/sun.jpg";
 
 const Home = () => {
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  };
   return (
-    <div className="w-full min-h-screen flex bg-blue-900 px-6 justify-between items-center">
-      <a>Kali-Docs</a>
-      <li>
-        <a>Docs</a>
-        <a>Donate</a>
-      </li>
+    <div
+      className="w-full min-h-screen flex bg-blue-900 px-6 justify-center"
+      style={divStyle}
+    >
+      <div className="flex w-full py-6 justify-center">
+        <a>Kali-Docs</a>
+        <li>
+          <a>Docs</a>
+          <a>Donate</a>
+        </li>
+      </div>
     </div>
   );
 };
