049c8a48ab24c500803c9a36f7591dfe39a6d3ad
Dr Code <dice@Dice>
2023-09-14 09:16:09 +0200

    first commit

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import kali1 from "../images/kali1.jpg";
+import kali2 from "../images/banner-kali-10-year.jpg"
 
 const News = () => {
   const [open, setOpen] = useState(false);
@@ -69,15 +70,19 @@ const News = () => {
           </button>
         </li>
       </div>
-      <div className="flex-grow flex justify-center items-center py-6">
-        <div className="w-full justify-center items-center flex flex-col">
-          <h1 className="text-[35px] font-bold text-blue-950">Kali Latest News</h1>
+      <div className="flex-grow flex flex-col justify-center items-center py-6">
+        <div className="w-[900px] justify-center items-center flex flex-col">
+          <h1 className="text-[35px] font-bold text-blue-950">
+            Kali Latest News
+          </h1>
           <div className="w-full justify-start flex flex-col">
             <h1 className="text-[30px] text-blue-900">
               Kali Linux 2023.3 Release (Internal Infrastructure & Kali
               Autopilot)
             </h1>
-            <p className="text-[15px] text-blue-800">Wednesday, 23 August 2023</p>
+            <p className="text-[15px] text-blue-800">
+              Wednesday, 23 August 2023
+            </p>
             <img className="object-cover rounded w-full" src={kali1} />
             <p className="text-[20px]">
               Today we are delighted to introduce our latest release of Kali,
@@ -89,7 +94,72 @@ const News = () => {
               If you want to see what’s new for yourself download or upgrade if
               you have an existing Kali Linux installation.
             </p>
-            <button className="text-[20px] py-2 px-2 bg-blue-700 w-[150px] rounded text-white">Read More</button>
+            <button className="text-[20px] py-2 px-2 bg-blue-700 w-[150px] rounded text-white">
+              Read More
+            </button>
+          </div>
+          <div className="w-full justify-start flex flex-col">
+            <h1 className="text-[30px] text-blue-900">
+              Happy 10th anniversary & Kali's story ...so far
+            </h1>
+            <p className="text-[15px] text-blue-800">29 March 2023</p>
+            <img className="object-cover rounded w-full" src={kali2} />
+            <p className="text-[20px]">
+              Wednesday 13th, March 2013, 10 years ago, Kali Linux v1.0 was
+              first released. Today we want to celebrate Kali’s 10th
+              anniversary! Time has flown. And gosh, a lot has changed since
+              then! They grow up so fast! This is the story of how Kali came to
+              be, and some of the challenges along the way.
+            </p>
+            <button className="text-[20px] py-2 px-2 bg-blue-700 w-[150px] rounded text-white">
+              Read More
+            </button>
+          </div>
+          <div className="w-full justify-start flex flex-col">
+            <h1 className="text-[30px] text-blue-900">
+              Kali Linux 2023.3 Release (Internal Infrastructure & Kali
+              Autopilot)
+            </h1>
+            <p className="text-[15px] text-blue-800">
+              Wednesday, 23 August 2023
+            </p>
+            <img className="object-cover rounded w-full" src={kali1} />
+            <p className="text-[20px]">
+              Today we are delighted to introduce our latest release of Kali,
+              2023.3. This release blog post does not have the most features in
+              it, as a lot of the changes have been behind-the-scenes, which
+              brings a huge benefit to us and an indirect positive effect to you
+              as end-users. It always goes without saying, but there are a
+              number of new packages and tools as well as the standard updates.
+              If you want to see what’s new for yourself download or upgrade if
+              you have an existing Kali Linux installation.
+            </p>
+            <button className="text-[20px] py-2 px-2 bg-blue-700 w-[150px] rounded text-white">
+              Read More
+            </button>
+          </div>
+          <div className="w-full justify-start flex flex-col">
+            <h1 className="text-[30px] text-blue-900">
+              Kali Linux 2023.3 Release (Internal Infrastructure & Kali
+              Autopilot)
+            </h1>
+            <p className="text-[15px] text-blue-800">
+              Wednesday, 23 August 2023
+            </p>
+            <img className="object-cover rounded w-full" src={kali1} />
+            <p className="text-[20px]">
+              Today we are delighted to introduce our latest release of Kali,
+              2023.3. This release blog post does not have the most features in
+              it, as a lot of the changes have been behind-the-scenes, which
+              brings a huge benefit to us and an indirect positive effect to you
+              as end-users. It always goes without saying, but there are a
+              number of new packages and tools as well as the standard updates.
+              If you want to see what’s new for yourself download or upgrade if
+              you have an existing Kali Linux installation.
+            </p>
+            <button className="text-[20px] py-2 px-2 bg-blue-700 w-[150px] rounded text-white">
+              Read More
+            </button>
           </div>
         </div>
       </div>
